51929cc813b2b0b11b7c1a5fb6a88fa4642b97b3
zfchen <zfchen916@163.com>
2020-10-26 14:04:14 +0800

    rocketMQTemplate.getProducer().getNamesrvAddr()

--- MyShopPlus/cloud/cloud-message-service/src/main/java/com/funtl/myshop/plus/cloud/controller/MessageController.java
+++ MyShopPlus/cloud/cloud-message-service/src/main/java/com/funtl/myshop/plus/cloud/controller/MessageController.java
@@ -1,15 +1,30 @@
 package com.funtl.myshop.plus.cloud.controller;
 
+import com.funtl.myshop.plus.cloud.dto.UmsAdminLoginLogDTO;
 import com.funtl.myshop.plus.cloud.producer.MessageProducer;
+import java.util.Date;
+import javax.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 @RestController
 @RequestMapping(value = "message")
 public class MessageController {
 
-    @Resource
-    private MessageProducer messageProducer;
+  @Resource
+  private MessageProducer messageProducer;
+
+
+  @GetMapping(value = "/demo")
+  public void demo() {
+    UmsAdminLoginLogDTO dto = new UmsAdminLoginLogDTO();
+    dto.setAdminId(1L);
+    dto.setAddress("白日依山尽,黄河入海流.欲穷千里目,更上一层楼.");
+    dto.setIp("192.168.1.220");
+    dto.setCreateTime(new Date());
+    dto.setUserAgent("花有重开日人无再少年");
+    messageProducer.sendAdminLoginLog(dto);
+  }
 }
--- MyShopPlus/cloud/cloud-message-service/src/test/java/com/funtl/myshop/plus/cloud/tests/MessageCloudTests.java
+++ MyShopPlus/cloud/cloud-message-service/src/test/java/com/funtl/myshop/plus/cloud/tests/MessageCloudTests.java
@@ -1,16 +1,22 @@
 package com.funtl.myshop.plus.cloud.tests;
+import com.funtl.myshop.plus.cloud.producer.MessageProducer;
 import java.util.Date;
 
 import com.funtl.myshop.plus.cloud.dto.UmsAdminLoginLogDTO;
 import com.funtl.myshop.plus.commons.utils.MapperUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MessageCloudTests {
+
+    @Autowired
+    private MessageProducer messageProducer;
 
     @Test
     public void testPrintln() throws Exception {
@@ -21,6 +27,17 @@ public class MessageCloudTests {
         dto.setAddress("0.0.0.0");
         dto.setUserAgent("0.0.0.0");
         System.out.println(MapperUtils.obj2json(dto));
+    }
+
+    @Test
+    public void demo() {
+        UmsAdminLoginLogDTO dto = new UmsAdminLoginLogDTO();
+        dto.setAdminId(1L);
+        dto.setAddress("白日依山尽,黄河入海流.欲穷千里目,更上一层楼.");
+        dto.setIp("192.168.1.220");
+        dto.setCreateTime(new Date());
+        dto.setUserAgent("花有重开日人无再少年");
+        messageProducer.sendAdminLoginLog(dto);
     }
 
 }
